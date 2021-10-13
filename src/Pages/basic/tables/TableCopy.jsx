b5ecf7d083ad7d667b82cde02b5d263af0e7531b
Gabor Javorszky <gabor.javorszky@ardanlabs.com>
2021-10-14 00:29:06 +0100

    Copy table half

--- src/Pages/basic/tables/TableCopy.jsx
+++ src/Pages/basic/tables/TableCopy.jsx
@@ -1,5 +1,13 @@
 export default function TablesCopy() {
     return (
-        <h1 className="title">Tables Copy</h1>
+        <>
+            <h2 className="subtitle">Copy a database table</h2>
+
+            <p>Sometimes we'll need to copy an existing database table's structure, without the content. Situations where you have a lot of data (hundreds of millions of rows), and you need to make some changes to the structure but you want to start with what's available, but you don't want to wait until the changes are applied to all rows, or simply you want to experiment without fear of destroying data.</p>
+
+
+            <p>In those cases we can use a <code>like</code> operator instead of manually specifying each individual columns.</p>
+        </>
+
     )
 }
