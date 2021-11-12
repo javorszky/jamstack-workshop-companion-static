bfbaa7334d5f024b151faa860dce8a5f1525ea0b
Gabor Javorszky <gabor@javorszky.co.uk>
2021-11-12 13:09:21 +0000

    Finish adding delete info

--- src/Pages/basic/delete/Overview.jsx
+++ src/Pages/basic/delete/Overview.jsx
@@ -1,5 +1,19 @@
+import { Link } from "react-router-dom"
+
 export default function DeleteOverview() {
     return (
-        <h1 className="title">Delete Overview</h1>
+        <>
+            <h2 className="subtitle mb-6">Deleting data</h2>
+
+            <p>Deletion is fairly straightforward. We're going to talk about three different and simple ways:</p>
+
+            <ul>
+                <li><Link to="/basic/delete/delete-one">Deleting a single row</Link></li>
+                <li><Link to="/basic/delete/delete-many">Deleting multiple rows at the same time</Link></li>
+                <li><Link to="/basic/delete/truncate">Deleting everything from a table</Link></li>
+            </ul>
+            <p>Take your pick :).</p>
+        </>
+
     )
 }
--- src/Pages/joins/Overview.jsx
+++ src/Pages/joins/Overview.jsx
@@ -1,5 +1,10 @@
 export default function JoinsOverview() {
     return (
-        <h1 className="title">Joins Overview</h1>
+        <>
+            <h2 className="subtitle mb-6">Table joins</h2>
+            <p>The whole reason for the existence of a relational database is the relationships part. The other side of that is the answer to the question: relationships between <i>what</i>?</p>
+
+            <p>To answer that question we need to talk about <i><b>normalization of data</b></i></p>
+        </>
     )
 }
